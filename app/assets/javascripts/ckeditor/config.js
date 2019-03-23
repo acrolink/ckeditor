2cd88d30db29a966059dc9ea51e24b6734eded1a
Waseem Massry <contact@acro.link>
2019-03-23 11:08:09 +0000

    bidi in conf

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -33,6 +33,8 @@ CKEDITOR.editorConfig = function( config )
 
   config.allowedContent = true;
 
+  config.extraPlugins = 'bidi';
+
   // Toolbar groups configuration.
   config.toolbar = [
     { name: 'document', groups: [ 'mode', 'document', 'doctools' ], items: [ 'Source'] },
